806e162ffcf0a03aac33f2128536c6a8628e32b5
keimaconquesta <47274035+keimaconquesta@users.noreply.github.com>
2020-01-21 10:33:03 +0800

    -Imported code from Internal Storage Project including XML

--- app/src/main/java/org/kcnq/externalfilestorage/MainActivity.java
+++ app/src/main/java/org/kcnq/externalfilestorage/MainActivity.java
@@ -3,12 +3,86 @@ package org.kcnq.externalfilestorage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int READ_BLOCK_SIZE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText callTextBox = findViewById(R.id.textBox);
+        final Button callClearButton = findViewById(R.id.clearButton);
+        final Button callReadButton = findViewById(R.id.readButton);
+        final Button callWriteButton = findViewById(R.id.writeButton);
+
+        callClearButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        callTextBox.setText("");
+                    }
+                }
+        );
+
+        callReadButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            FileInputStream fileIn=openFileInput("mytextfile.txt");
+                            InputStreamReader InputRead= new InputStreamReader(fileIn);
+
+                            char[] inputBuffer= new char[READ_BLOCK_SIZE];
+                            String s="";
+                            int charRead;
+
+                            while ((charRead=InputRead.read(inputBuffer))>0) {
+                                // char to string conversion
+                                String readstring=String.copyValueOf(inputBuffer,0,charRead);
+                                s +=readstring;
+                            }
+                            InputRead.close();
+                            callTextBox.setText(s);
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+
+        callWriteButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            FileOutputStream fileout=openFileOutput("mytextfile.txt", MODE_PRIVATE);
+                            OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
+                            outputWriter.write(callTextBox.getText().toString());
+                            outputWriter.close();
+
+                            //display file saved message
+                            Toast.makeText(getBaseContext(), "File saved successfully!",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
     }
 }
